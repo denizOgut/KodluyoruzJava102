d3c40e1848765158b6ec74631a6e116c6e536fca
Deniz Öğüt <denizogut123@gmail.com>
2022-01-17 11:16:54 +0300

    Login ekranı bitti, Eğitmen Paneline başlandı.

--- Java ile Backend Web Development/Java102/PatikaDevClone/src/com/patikadev/Model/Operator.java
+++ Java ile Backend Web Development/Java102/PatikaDevClone/src/com/patikadev/Model/Operator.java
@@ -1,5 +1,11 @@
 package com.patikadev.Model;
 
+import com.patikadev.Helper.DBConnecter;
+import com.patikadev.Helper.Helper;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Operator extends User{
     public Operator() {
     }
@@ -7,4 +13,36 @@ public class Operator extends User{
     public Operator(String name, String surname, String username, String type, int id, String password) {
         super(name, surname, username, type, id, password);
     }
+
+    public static int addOperator(Operator operator) throws SQLException {
+        String type = "operator";
+        int result;
+        String columnNames[] = new String[]{"id"};
+        String query = "INSERT INTO public.\"user\"(\n" +
+                "\tusername, surname, name,  type, password)\n" +
+                "\tVALUES (?, ?, ?,?, ?);";
+        User userCheck = User.getFetch(operator.getUsername());
+        if (userCheck != null) {
+            Helper.showMessage("Duplicate Kayıt");
+            result = -1;
+            return result;
+        }
+
+        PreparedStatement preparedStatement = DBConnecter.getInstance().prepareStatement(query, columnNames);
+        preparedStatement.setString(1, operator.getUsername());
+        preparedStatement.setString(2, operator.getSurname());
+        preparedStatement.setString(3, operator.getName());
+        preparedStatement.setString(4, type);
+        preparedStatement.setString(5, operator.getPassword());
+        result = preparedStatement.executeUpdate();
+        if (result == 1) {
+            java.sql.ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                operator.setId(generatedKeys.getInt(1));
+                Helper.showMessage("success");
+            }
+        }
+        return result;
+    }
+
 }
--- Java ile Backend Web Development/Java102/PatikaDevClone/src/com/patikadev/View/LoginGUI.java
+++ Java ile Backend Web Development/Java102/PatikaDevClone/src/com/patikadev/View/LoginGUI.java
@@ -11,61 +11,71 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
-import java.util.Arrays;
 
 public class LoginGUI extends JFrame {
-    private JPanel wrapper;
+    private JPanel wrapperLogin;
     private JPanel wTop;
     private JPanel wBottom;
     private JTextField fld_user_username;
     private JPasswordField fld_user_parola;
     private JButton btn_user_login;
-    private JTabbedPane tabbedPane1;
     private JTextField fld_student_name;
     private JTextField fld_student_surname;
     private JTextField fld_student_uname;
     private JTextField fld_student_parola;
     private JButton btn_signIn;
+    private JTabbedPane fld_tabbed_pane;
+    private JTextField fld_operator_name;
+    private JTextField fld_operator_surname;
+    private JTextField fld_operator_username;
+    private JTextField fld_operator_parola;
+    private JButton btn_operator_kaydet;
 
     public LoginGUI() throws HeadlessException {
-        add(wrapper);
+        add(wrapperLogin);
         setSize(200, 200);
         setSize(new Dimension(1000, 500));
         this.setLocation(Helper.screenSize.width / 2 - this.getSize().width / 2, Helper.screenSize.height / 2 - this.getSize().height / 2);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setTitle(Config.PROJECT_TITLE);
         setVisible(true);
-
+        fld_tabbed_pane.setEnabledAt(2, false);
 
         btn_user_login.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (fld_user_username.getText().isEmpty() || fld_user_parola.getText().isEmpty()) {
-                    Helper.showMessage("fill");
-                } else {
-                    try {
-                        User user = User.getFetch(fld_user_username.getText(), fld_user_parola.getText());
-                        if (user == null) {
-                            Helper.showMessage("Kullanıcı Bulunamadı");
-                        } else {
-                            switch (user.getType()) {
-                                case "operator":
-                                    OperatorGUI operatorGUI = new OperatorGUI((Operator) user);
-                                    break;
-                                case "educator":
-                                    EducatorGUI educatorGUI = new EducatorGUI();
-                                    break;
-                                case "student":
-                                    StudentGUI studentGUI = new StudentGUI((Student) user);
-                                    break;
-                            }
-                            dispose();
 
+                if (fld_user_username.getText().equals("admin") && fld_user_parola.getText().equals("admin")) {
+                    fld_tabbed_pane.setEnabledAt(2, true);
+                } else {
+                    if (fld_user_username.getText().isEmpty() || fld_user_parola.getText().isEmpty()) {
+                        Helper.showMessage("fill");
+                    } else {
+                        try {
+                            User user = User.getFetch(fld_user_username.getText(), fld_user_parola.getText());
+                            if (user == null) {
+                                Helper.showMessage("Kullanıcı Bulunamadı");
+                            } else {
+                                switch (user.getType()) {
+                                    case "operator":
+                                        OperatorGUI operatorGUI = new OperatorGUI((Operator) user);
+                                        break;
+                                    case "educator":
+                                        EducatorGUI educatorGUI = new EducatorGUI();
+                                        break;
+                                    case "student":
+                                        StudentGUI studentGUI = new StudentGUI((Student) user);
+                                        break;
+                                }
+                                wrapperLogin.setVisible(false);
+
+                            }
+                        } catch (SQLException | UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+                            ex.printStackTrace();
                         }
-                    } catch (SQLException | UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-                        ex.printStackTrace();
                     }
                 }
+
             }
         });
         btn_signIn.addActionListener(new ActionListener() {
@@ -91,18 +101,57 @@ public class LoginGUI extends JFrame {
                 fld_student_uname.setText(null);
                 fld_student_parola.setText(null);
                 StudentGUI studentGUI = new StudentGUI(student);
-                dispose();
+               dispose();
 
 
             }
         });
 
+        btn_operator_kaydet.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Operator operator = new Operator();
+                if (fld_operator_name.getText().isEmpty() || fld_operator_surname.getText().isEmpty() || fld_operator_username.getText().isEmpty() ||
+                        fld_operator_parola.getText().isEmpty()) {
+                    Helper.showMessage("fill");
+                } else {
+                    operator.setName(fld_operator_name.getText());
+                    operator.setPassword(fld_operator_parola.getText());
+                    operator.setUsername(fld_operator_username.getText());
+                    operator.setSurname(fld_operator_surname.getText());
+                    try {
+                        Operator.addOperator(operator);
+                    } catch (SQLException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                fld_student_name.setText(null);
+                fld_student_surname.setText(null);
+                fld_student_uname.setText(null);
+                fld_student_parola.setText(null);
+                try {
+                    OperatorGUI operatorGUI = new OperatorGUI(operator);
+                } catch (UnsupportedLookAndFeelException ex) {
+                    ex.printStackTrace();
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace();
+                } catch (InstantiationException ex) {
+                    ex.printStackTrace();
+                } catch (IllegalAccessException ex) {
+                    ex.printStackTrace();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+                dispose();
+            }
+        });
     }
-        public static void main (String[]args){
-            LoginGUI loginGUI = new LoginGUI();
+
+    public static void main(String[] args) {
+        LoginGUI loginGUI = new LoginGUI();
 
 
-        }
     }
+}
 
 
